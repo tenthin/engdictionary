1dd6e1917b39bb83154539805d202d706bc25b72
Tenzin <tenthinten@gmail.com>
2021-12-31 14:58:53 +0530

    add dictionary libraries and import it

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from PyDictionary import PyDictionary
+import dictionary
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def word(request):
+    search = request.GET.get('search')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+    synonyms = dictionary.synonyms(search)
+    antonyms = dictionary.antonyms(search)
+    context = {
+        'meaning': meaning,
+        'synonyms': synonyms,
+        'antonyms': antonyms
+    }
+    return render (request, 'word.html')
