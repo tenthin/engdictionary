624773d338b8ed45186fb67076b842abf469936a
Tenzin <tenthinten@gmail.com>
2021-12-31 15:22:13 +0530

    checking for antonymns and synonyms

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from PyDictionary import PyDictionary
 
-
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -13,8 +12,8 @@ def word(request):
     synonyms = dictionary.synonym(search)
     antonyms = dictionary.antonym(search)
     context = {
-        'meaning': meaning,
+        'meaning': meaning['Noun'][0],
         'synonyms': synonyms,
         'antonyms': antonyms
     }
-    return render (request, 'word.html',context)
+    return render(request, 'word.html', context)
